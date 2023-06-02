246d7f3d8b435539c9d876ae6297876b4ff1f4bd
Fredy Aristizabal <freddyarh13@gmail.com>
2023-06-02 15:40:28 +1000

    Custom Hook for the operators

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,125 +1,24 @@
-import { useRef, useState } from "react";
+
 import { Text, View } from "react-native";
 import { BotonCalculator } from "../components/BotonCalculator";
 import { styles } from "../theme/appTheme";
-
-enum Operators {
-  add, sub, mul, spl
-}
+import { useCalculator } from "../hooks/useCalculator";
 
 export const CalculatorScreen = () => {
 
-  const [number, setNumber] = useState('100');
-  const [numberBefore, setNumberBefore] = useState('0');
-
-  const lastOperation = useRef<Operators>();
-
-  const clean = () => {
-    setNumber('0');
-    setNumberBefore('0');
-  }
-
-  const createNumber = ( numberText: string ) => {
-
-    if( number.includes('.') && numberText === '.' ) return;
-
-        if ( number.startsWith('0') || number.startsWith('-0') ) {
-
-            // Punto decimal
-            if ( numberText === '.' ) {
-                setNumber( number + numberText );
-
-                // Evaluar si es otro cero, y hay un punto
-            } else if( numberText === '0' && number.includes('.')  ) {
-                setNumber( number + numberText );
-
-                // Evaluar si es diferente de cero y no tiene un punto
-            } else if( numberText !== '0' && !number.includes('.') ) {
-                setNumber( numberText );
-
-                // Evitar 0000.0
-            } else if( numberText === '0' && !number.includes('.') ) {
-                setNumber( number );
-            } else {
-                setNumber( number + numberText ); 
-            }
-
-        } else {
-            setNumber( number + numberText );
-        }
-  }
-
-  const positiveNegative = () => {
-    if ( number.includes('-') ) {
-      setNumber( number.replace('-', '') );
-    } else {
-        setNumber( '-' + number );
-    }
-  }
-
-  const btnDel = () => {
-
-    if( number.length === 1 || (number.length === 2 && number.includes('-')) ){
-      setNumber( '0' );
-    }else{
-      setNumber( number.slice( 0,  -1) )
-    }
-  }
-
-  const changeNumtoBefore = () => {
-    if( number.endsWith('.') ){
-      setNumber( number.slice(0, -1) );
-    }else{
-      setNumberBefore( number );
-    }
-
-    setNumber( '0' );
-  }
-
-  const btnAdd = () => {
-    changeNumtoBefore();
-    lastOperation.current = Operators.add;
-    console.log(lastOperation.current)
-  }
-  const btnSub = () => {
-    changeNumtoBefore();
-    lastOperation.current = Operators.sub;
-  }
-  const btnMul = () => {
-    changeNumtoBefore();
-    lastOperation.current = Operators.mul;
-  }
-  const btnSpl = () => {
-    changeNumtoBefore();
-    lastOperation.current = Operators.spl;
-  }
-
-  const result = () => {
-
-    const num1 = Number(number);
-    const num2 = Number(numberBefore);
-
-    switch ( lastOperation.current ) {
-      case Operators.add:
-          setNumber(`${num2 + num1}`);
-        break;
-      case Operators.sub:
-          setNumber(`${num2 - num1}`);
-        break;
-      case Operators.mul:
-          setNumber(`${num2 * num1}`);
-        break;
-      case Operators.spl:
-          setNumber(`${num2 / num1}`);
-        break;
-    
-      default:
-        break;
-    }
-
-    setNumberBefore('0');
-
-  }
+  const {
+      number,
+      numberBefore,
+      clean,
+      positiveNegative,
+      btnDel,
+      btnSpl,
+      createNumber,
+      btnMul,
+      btnSub,
+      btnAdd,
+      result,
+  } = useCalculator();
 
   return (
     <View style={ styles.calculadoraContainer }>
